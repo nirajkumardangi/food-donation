5642d5eb303f9f6b50d511eea5db98d10fb5242a
Raushankumar saw <raushankumarsaw15@gmail.com>
2024-07-05 04:24:53 +0530

    folder structure changed Raushan

--- src/components/Home/InfoSection.jsx
+++ src/components/Home/InfoSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import "./InfoSection.css";
 
 const InfoSection = () => {
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Footer from '../components/Home/Footer'
+import Header from "../components/Home/Header";
+import InfoSection from "../components/Home/InfoSection";
+import HeroSection from "../components/Home/HeroSection";
+
+
+
+export default function Home(){
+    return (
+      <>
+       
+        <Header/>
+       <HeroSection/>
+        <InfoSection/>
+     <Footer/>
+      </>
+    );
+}
