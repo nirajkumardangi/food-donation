187c5418e71c15d0f71a9779b819aad644c58a4b
Raushankumar saw <raushankumarsaw15@gmail.com>
2024-07-05 19:01:56 +0530

    little bugfix

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,21 +1,9 @@
-import {userInfoAction} from '../utility/Storage'
-import {useDispatch} from 'react-redux'
-
-
-
 function Blog() {
- const dispatch=useDispatch()
-function handle(){
-dispatch(userInfoAction.login())
-  
-}
-   
   return (
     <>
       <div>this is a dummy Blog page</div>
-      <button onClick={handle}>login</button>
     </>
   );
 }
 
-export default Blog
+export default Blog;
