12f1413faf166609ced8a75d50c326a13e51c5b4
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-13 10:40:29 +0530

    fix donation page error

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import HeroPageSlider from "./HeroPageSlider";
+import { useFirebase } from "../../utility/Storage";
 
 const HeroSection = () => {
+  const firebase = useFirebase();
   return (
     <section className="hero-section relative h-screen overflow-hidden mt-18">
       <HeroPageSlider />
@@ -16,7 +18,7 @@ const HeroSection = () => {
             can make a difference in the lives of many.
           </p>
           <Link
-            to="/auth"
+            to={firebase.isLogin ? "/donation/form" : "/auth"}
             className="bg-primary-color hover:bg-secondary-color px-6 py-3 rounded-lg text-lg font-light-bold transition-colors duration-500"
           >
             Donate Now
